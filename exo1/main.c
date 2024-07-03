1075eabab9da398dfa8ab4026e92de123d7881c3
Kapi4Real <charly.couton233@gmail.com>
2024-07-03 22:23:45 +0200

    mise a jour de main.c

--- exo1/main.c
+++ exo1/main.c
@@ -1,9 +1,48 @@
-#include<stdio.h>
+#include<stdio.h> 
 #include<string.h>
+#include<stdlib.h>
 
 
 int main(void)
-{
+{	
+	char **tableau;
+	int raws =0;
+	int lenght= 0;
+	int i=0;
+	int j=0;
+
+	printf("Largeur du tableau :\n");
+	scanf("%d",&raws);
+	printf("Longueur du tableau :\n");
+        scanf("%d",&lenght);
+
+
+	tableau = malloc(sizeof(char*)*raws);
+
+	while(i<lenght)
+	{
+		tableau[i] = malloc(sizeof(char)*lenght);
+		i++;
+	}
+
 	
+	for(i=0;i<raws;i++)
+	{
+		for(j=0;j<raws;j++)
+		{
+		 tableau[i][j]='O';
+		 printf("%c",tableau[i][j]);
+		}
+	printf("\n");
+	}
+	
+	i=0;
+	while(i<raws)
+	{	
+		free(tableau[i]);
+		i++;
+	}
+	
+	free(tableau);
 return 0;
 }
